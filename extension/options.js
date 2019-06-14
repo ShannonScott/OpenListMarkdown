0243f78f212ae58e0e6d36af82ec80257b553ae3
Shannon Scott <shannon.scott@jemeztechnology.com>
2019-06-14 16:10:51 -0600

    Code clean-up

--- extension/options.js
+++ extension/options.js
@@ -1,25 +1,12 @@
 // Saves options to chrome.storage
 function save_options() {
-  var formatValue = document.getElementById('format').value;
-  /*chrome.storage.sync.set({formatType: formatType}, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });*/
-  localStorage.setItem('format', formatValue)
+  // Save format to local storage
+  localStorage.setItem('format', document.getElementById('format').value)
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Restores select box and checkbox state
 function restore_options() {
-  chrome.storage.sync.get({
-    formatType: 'markdown',
-  }, function(items) {
-    document.getElementById('format').value = items.formatType;
-  });
+  document.getElementById('format').value = localStorage.getItem('format')
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
